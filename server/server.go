00b01ef0995bd270e2b61e61686a147c538601a1
ryanolsen <ryanolsen5@gmail.com>
2013-08-14 08:10:17 -0600

    merged upstream and view content

--- server/server.go
+++ server/server.go
@@ -11,8 +11,22 @@ func main() {
 	// Locations where static files reside.
 	ServeStaticFiles("/public/", "/views/")
 
+	http.HandleFunc("/api/getUser/", getUser)
+
+
 	// Start Server on Port
 	port := strconv.FormatInt(8080, 10)
 	fmt.Println("Starting server on ", port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+// ======================= Test ======================= //
+
+func getUser(w http.ResponseWriter, r *http.Request){
+	h := w.Header()
+	h["Content-Type"] = []string { "application/json", "charset-utf-8", }	
+
+	s := "[{name:'John', email:'John Smith'}]"
+
+	fmt.Fprint(w, s)
 }
