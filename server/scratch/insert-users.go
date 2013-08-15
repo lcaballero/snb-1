56eb89cca392a9daadbb8bd8849db818229c38fe
Lucas Caballero <caballero.luke@gmail.com>
2013-08-15 07:49:45 -0600

    Committing some changes before merging with ryan's code.

---
+++ server/scratch/insert-users.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Start running inserts")
+}
+
+func creatUser(id, version, usergroup_id int, password, username string) {
+	sql := `
+insert into _user
+	(id, version, usergroup_id, password, username)
+values
+	($1, $2, $3, $4, $5);
+`
+	result, err := getConnection().Exec(sql, id, version, usergroup_id, password, username)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+}
