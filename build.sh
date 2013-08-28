eb193bbbec9fbab960c129c17db8c5da56c68312
Lucas Caballero <caballero.luke@gmail.com>
2013-08-28 08:35:52 -0600

    Updated to set gopath relative to the root directory.

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
+# Set the GOPATH so that we can reference code in the current
+# project without having to update the GOPATH for specifically
+# to where we've put the project directory
+
+# Save the current GOPATH to later restore it to this value.
+# If we continually add to the GOPATH we'd wind up adding PWD
+# every time we ran this script -- which is messy.
+TEMP_GOPATH=GOPATH
+GOPATH=$PWD/server/libs/:$GOPATH
+
+# Actual 'build' command that we want to run
 go build -o web-server.exe server/server.go server/headers.go server/all-users.go
+
+# Restoring the old GOPATH
+GOPATH=TEMP_GOPATH
+
